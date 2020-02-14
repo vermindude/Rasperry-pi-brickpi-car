8b38ba1406b379444c43aa29066f551433b849eb
vermindude <vermin.dude@gmail.co,m>
2020-02-13 18:55:44 -0600

    adding car

--- car.py
+++ car.py
@@ -11,7 +11,7 @@
 #
 # Hardware: Connect EV3 or NXT motor(s) to any of the BrickPi3 motor ports. Make sure that the BrickPi3 is running on a 9v power supply.
 #
-# Results:  When-0ou run this program, the motor(s) speed will ramp up and down while the touch sensor is pressed. The position for each motor will be printed.
+# Results:  When you run this program, the motor(s) speed will ramp up and down while the touch sensor is pressed. The position for each motor will be printed.
 
 from __future__ import print_function  # use python 3 syntax but make it compatible with python 2
 from __future__ import division  # ''
@@ -19,12 +19,12 @@ from __future__ import division  # ''
 import time  # import the time library for the sleep function
 import brickpi3  # import the BrickPi3 drivers
 
-
 BP = brickpi3.BrickPi3()  # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
 
 BP.set_sensor_type(BP.PORT_1,
                    BP.SENSOR_TYPE.TOUCH)  # Configure for a touch sensor. If an EV3 touch sensor is connected, it will be configured for EV3 touch, otherwise it'll configured for NXT touch.
 
+print("HELLOOOO")
 try:
     print("Press touch sensor on port 1 to run motors")
     value = 0
@@ -42,34 +42,31 @@ try:
         # BP.get_sensor returns the sensor value.
         try:
             value = BP.get_sensor(BP.PORT_1)
-            print("YAY!!! GOT THE DAMN VALUE FROM THE SENSOR ON BP>PORT_1")
+            print("VAL CHANGED TO ", value)
         except brickpi3.SensorError as error:
-            print(error)
+            print("ERROR:", error)
             value = 0
 
-        print("VALUE IS:", str(value))
-
         if value:  # if the touch sensor is pressed
-            BP.set_motor_power(BP.PORT_A, speed)
-            time.sleep(3)
+            if speed <= -100 or speed >= 100:  # if speed reached 100, start ramping down. If speed reached -100, start ramping up.
+                adder = -adder
+            speed += adder
         else:  # else the touch sensor is not pressed or not configured, so set the speed to 0
             speed = 0
             adder = 1
-
+        print ("GOT HERE")
         # Set the motor speed for all four motors
-        BP.set_motor_power(BP.PORT_A, speed)
+        BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, speed)
+
         try:
             # Each of the following BP.get_motor_encoder functions returns the encoder value (what we want to display).
             print("Encoder A: %6d  B: %6d  C: %6d  D: %6d" % (
-                BP.get_motor_encoder(BP.PORT_A), BP.get_motor_encoder(BP.PORT_B), BP.get_motor_encoder(BP.PORT_C),
-                BP.get_motor_encoder(BP.PORT_D)))
+            BP.get_motor_encoder(BP.PORT_A), BP.get_motor_encoder(BP.PORT_B), BP.get_motor_encoder(BP.PORT_C),
+            BP.get_motor_encoder(BP.PORT_D)))
         except IOError as error:
             print(error)
-
 
         time.sleep(0.02)  # delay for 0.02 seconds (20ms) to reduce the Raspberry Pi CPU load.
 
 except KeyboardInterrupt:  # except the program gets interrupted by Ctrl+C on the keyboard.
     BP.reset_all()  # Unconfigure the sensors, disable the motors, and restore the LED to the control of the BrickPi3 firmware.
-
-
