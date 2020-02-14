71df9d128cd565653c111d791e0039517c99beb4
vermindude <vermin.dude@gmail.co,m>
2020-02-13 18:16:08 -0600

    adding car

--- car.py
+++ car.py
@@ -67,3 +67,4 @@ try:
 except KeyboardInterrupt:  # except the program gets interrupted by Ctrl+C on the keyboard.
     BP.reset_all()  # Unconfigure the sensors, disable the motors, and restore the LED to the control of the BrickPi3 firmware.
 
+
