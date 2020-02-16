c5e389842324935a69840da3e4d8cf658cdc7a14
vermindude <vermin.dude@gmail.co,m>
2020-02-16 15:20:51 -0600

    adding car

--- run_car.py
+++ run_car.py
@@ -39,6 +39,7 @@ try:
     print("the button was pressed!!!")
     power = 10
     BP.set_motor_power(BP.PORT_A, power)
+    print("IT WENT !!!")
 
 except KeyboardInterrupt:  # except the program gets interrupted by Ctrl+C on the keyboard.
     BP.reset_all()  #    BP.set_motor_power(BP.PORT_A, power)
