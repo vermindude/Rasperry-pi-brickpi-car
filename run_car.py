1a02fe34dda86b60b43b51c161d74211d5ea6d99
vermindude <vermin.dude@gmail.co,m>
2020-02-18 16:28:49 -0600

    adding car

--- run_car.py
+++ run_car.py
@@ -37,7 +37,7 @@ try:
         except brickpi3.SensorError:
             pass
     print("the button was pressed!!!")
-    power = 500
+    power = 500, BP.speed(0)
     BP.set_motor_power(BP.PORT_A, power)
     print("IT WENT !!!")
 
