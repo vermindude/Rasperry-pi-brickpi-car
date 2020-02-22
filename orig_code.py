c7bbafa8560311848b7e7d533855d56ff4613d26
vermindude <vermin.dude@gmail.co,m>
2020-02-21 19:52:19 -0600

    adding car

--- orig_code.py
+++ orig_code.py
@@ -59,6 +59,6 @@ try:
             print(error)
 
         time.sleep(0.02)  # delay for 0.02 seconds (20ms) to reduce the Raspberry Pi CPU load.
-
+    BP.reset_all()
 except KeyboardInterrupt:  # except the program gets interrupted by Ctrl+C on the keyboard.
     BP.reset_all()  # Unconfigure the sensors, disable the motors, and restore the LED to the control of the BrickPi3 firmware.
